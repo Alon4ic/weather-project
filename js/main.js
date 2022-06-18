a1501097e3fd9259dcad76bae7a0e33a124235d6
Alon4ic <o.redjko@gmail.com>
2022-06-18 15:40:51 +0300

    My project

--- js/main.js
+++ js/main.js
@@ -33,13 +33,13 @@ let newDate = currentDate.getDate();
 function formatDay(timestamp) {
 	let date = new Date(timestamp * 1000);
 	let day = date.getDay();
-	let days = ["Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"];
+	let days = ["Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat"];
 	return days[day];
 	
 }
